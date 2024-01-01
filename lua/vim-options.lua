16708706358d83a53652cc69c39db73c79e31d89
Aaron Zirfas <CrimsonKiss@iMac-27.local>
2024-01-01 12:05:39 +0100

    added none-ls plugin to format

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -19,7 +19,7 @@ opt.hlsearch = false
 -- Appeaance
 opt.relativenumber = true
 opt.termguicolors = true
-opt.colorcolumn = '100'
+opt.colorcolumn = "100"
 opt.signcolumn = "yes"
 opt.scrolloff = 10
 opt.completeopt = "menuone,noinsert,noselect"
@@ -35,7 +35,7 @@ opt.splitright = true
 opt.splitbelow = true
 opt.autochdir = false
 opt.iskeyword:append("-")
-opt.mouse:append('a')
+opt.mouse:append("a")
 opt.clipboard:append("unnamedplus")
 opt.modifiable = true
 opt.encoding = "UTF-8"
